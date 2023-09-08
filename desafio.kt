b1b76c66619415673c2841579c0c1c7ead9f0c00
Marcos Alves <marcoveizalvess@gmail.com>
2023-09-08 18:30:28 -0300

    Solução da abstração do domínio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,56 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario (val nome: String, val id: Int)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+
+    fun desmatricular(usuario: Usuario){
+        inscritos.remove(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    // Simular a criação de usuarios
+    val usuario1 = Usuario("Marcos", 123)
+    val usuario2 = Usuario("Vinicius", 254)
+    val usuario3 = Usuario("Alves", 476)
+
+    // Simular a criação de conteudo educacional
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 30)
+    val conteudo2 = ConteudoEducacional("Avançando no Kotlin", 90)
+    val conteudo3 = ConteudoEducacional("Utilizando Kotlin no desenvolvimento Android", 60)
+    val conteudo4 = ConteudoEducacional("Jetpack completo", 180)
+    val conteudo5 = ConteudoEducacional("Android Studio Completo", 30)
+
+    // Simular a criação de uma formação
+    val formacaoKotlin = Formacao("Kotlin em aplicaçãos Android", listOf(conteudo1, conteudo2, conteudo3), Nivel.INTERMEDIARIO)
+    val formacaoJetpack = Formacao("Avançando no desenvolvimento Android com Jetpack", listOf(conteudo4, conteudo5), Nivel.AVANCADO)
+    
+    // Simular a matricula dos usuarios em alguma trilha de formação
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+    formacaoJetpack.matricular(usuario3)
+
+    // Simular exibição da lista usuarios matriculados
+    println("Inscritos na formação ${formacaoKotlin.nome}: ${formacaoKotlin.inscritos.map { it.nome }}")
+    println("Inscritos na formação ${formacaoJetpack.nome}: ${formacaoJetpack.inscritos.map { it.nome }}")
+    
+    // Simular o cancelamento da matrícula
+    formacaoKotlin.desmatricular(usuario2)
+    formacaoJetpack.matricular(usuario2)
+    println("")
+    println("Lista de inscritos atualizadas após cancelamento/mudança de curso:")
+
+    // Simular exibição da lista de usuarios após cancelamento de matrícula
+    println("Inscritos na formação ${formacaoKotlin.nome}: ${formacaoKotlin.inscritos.map { it.nome }}")
+    println("Inscritos na formação ${formacaoJetpack.nome}: ${formacaoJetpack.inscritos.map { it.nome }}")
 }
